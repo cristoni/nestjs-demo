917817a7a0a95ca1a6de5abbe0923d631ded7583
Gabriele Cristoni <gabrielecristoni@gmail.com>
2018-11-01 17:42:37 +0100

    - configured and implemented ConfigModule

---
+++ src/config/config.service.ts
@@ -0,0 +1,42 @@
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(['development', 'production', 'test', 'provision'])
+        .default('development'),
+      DB_HOST: Joi.string().hostname().required(),
+      DB_PORT: Joi.number().required(),
+      DB_USERNAME: Joi.string().required(),
+      DB_PASSWORD: Joi.string().allow('').required(),
+      DB_NAME: Joi.string().required(),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
+  }
+}
